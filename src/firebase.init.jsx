e7952c15f78c4fd2b7fcfee4d05449ca261b280e
SShipon <nxshipon@gmail.com>
2022-08-19 19:44:29 +0600

    code:fire:

---
+++ src/firebase.init.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKyJy8MYL8G0d-Q6wef0IBRXhZRh8W_DQ",
+  authDomain: "watch-product-website.firebaseapp.com",
+  projectId: "watch-product-website",
+  storageBucket: "watch-product-website.appspot.com",
+  messagingSenderId: "143103510014",
+  appId: "1:143103510014:web:cccd32ab20c203d3316caf",
+  measurementId: "G-L0J510HVJS",
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
